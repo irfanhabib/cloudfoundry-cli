7c76723bab5b05d6c360dcf7ee402925c65530af
Simon Leung <leungs@us.ibm.com>
2014-09-29 17:57:28 -0700

    Implement 'plugins' to list all installed plugin methods and the executable they belong to.
    [Finishes #78235118]
    
    Signed-off-by: Dan Lavine <dlavine@us.ibm.com>
    
    Former-commit-id: 2ed3a0d76ecb96ce2f27ef53bba3addfa22dd5fb

--- fixtures/plugins/test_2.go
+++ fixtures/plugins/test_2.go
@@ -15,30 +15,33 @@ import (
 
 type CliPlugin struct{}
 
+var commands = []string{"test_2_cmd1", "test_2_cmd2"}
+
 func (c *CliPlugin) Run(args string, reply *bool) error {
-	if args == "test1" {
-		theFirstPush()
-	} else if args == "test2" {
-		theSecondPush()
+	if args == "test_2_cmd1" {
+		theFirstCmd()
+	} else if args == "test_2_cmd2" {
+		theSecondCmd()
 	}
+	return nil
+}
+
+func (c *CliPlugin) ListCmds(args string, cmdList *[]string) error {
+	*cmdList = commands
 	return nil
 }
 
 func (c *CliPlugin) CmdExists(args string, exists *bool) error {
-	if args == "test1" || args == "test2" {
-		*exists = true
-	} else {
-		*exists = false
-	}
+	*exists = plugin.CmdExists(args, commands)
 	return nil
 }
 
-func theSecondPush() {
-	fmt.Println("HaHaHaHa you called THE SECOND TEST")
+func theFirstCmd() {
+	fmt.Println("You called cmd1 in test_2")
 }
 
-func theFirstPush() {
-	fmt.Println("HaHaHaHa you called THE FIRST TEST")
+func theSecondCmd() {
+	fmt.Println("You called cmd2 in test_2")
 }
 
 func main() {
